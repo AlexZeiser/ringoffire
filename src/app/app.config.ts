4ae839069ed75da88748e0fa814e093dfc0da2b3
Alex Zeiser <alex.zeiser.rest@gmail.com>
2024-07-31 14:44:15 +0200

    release 2.4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-70f77","appId":"1:337853689486:web:f0c75916d494374710e3df","databaseURL":"https://ring-of-fire-70f77-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"ring-of-fire-70f77.appspot.com","apiKey":"AIzaSyApI3vkZgrcH6vMYzyy58Lhbxsv5d_igtM","authDomain":"ring-of-fire-70f77.firebaseapp.com","messagingSenderId":"337853689486"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyApI3vkZgrcH6vMYzyy58Lhbxsv5d_igtM",
+    authDomain: "ring-of-fire-70f77.firebaseapp.com",
+    databaseURL: "https://ring-of-fire-70f77-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ring-of-fire-70f77",
+    storageBucket: "ring-of-fire-70f77.appspot.com",
+    messagingSenderId: "337853689486",
+    appId: "1:337853689486:web:f0c75916d494374710e3df"
+  }
+};
